6581c1954f3ee454f4aa072bbd3d01f3cfecc4c3
vuanh <vuanhh96@gmail.com>
2018-09-07 00:14:12 +0700

    07/09/18

--- Vtteck_homework/pyspark/ReadCSV/generate_data_pyspark.py
+++ Vtteck_homework/pyspark/ReadCSV/generate_data_pyspark.py
@@ -1,11 +1,14 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from pyspark.ml.linalg import SparseVector, DenseVector
+from scipy.sparse import csr_matrix
 import time
 
 s = time.time()
-spark = SparkSession\
-    .builder\
-    .appName("read_csv")\
+# Initialize spark session
+spark = SparkSession \
+    .builder \
+    .appName("read_csv") \
     .getOrCreate()
 
 schema = StructType([
@@ -23,25 +26,80 @@ schema = StructType([
     StructField("DurationVoice", IntegerType())
 ])
 
-data_dir = 'datasource/'
+data_dir = 'datasource'
 
-# read csv file to dataframe
-df = spark.read.csv(data_dir + 'All_User*.csv', sep=',', schema=schema, header=True)
+# Read csv file to dataframe
+df = spark.read.csv(data_dir + '/' + '*.csv', sep=',', header=True, schema=schema)
 df.show()
 
-# determine max values of VolData, NoSMS, DurationVoice
+# Determine max values of VolData, NoSMS, DurationVoice
+# option 1:
 max_values = df.groupBy().max('VolData', 'NoSMS', 'DurationVoice').collect()[0]
+
+# option 2:
+# df.createOrReplaceTempView('user')
+# max_values = spark.sql('MAX(VolData), MAX(NoSMS), MAX(DurationVoice) FROM user').collect()[0]
+
 print(max_values)
 
 # Min-max value scale of VolData, NoSMS, DurationVoice
-df = df.withColumn('VolData', df['VolData']/max_values[0]) \
+df = df.withColumn('VolData', df['VolData'] / max_values[0]) \
     .withColumn('NoSMS', df['NoSMS'] / max_values[1]) \
-    .withColumn('DurationVoice', df['DurationVoice']/max_values[2])
+    .withColumn('DurationVoice', df['DurationVoice'] / max_values[2])
 df.show()
 
+# Create features formatted to vector[504]
+n_features = 3  # number of features used
 
+
+def parse(x):
+    """
+    map each record x according to format with
+    key = ('ID_USER','WEEK) and
+    value = [(idx1, 'VolData'), (idx2, 'NoSMS'), (idx3, 'DurationVoice')]
+    - idx* is corresponding index in vector[504] to create sparse vector
+    :param x:
+    :return: (key, value)
+    """
+    key = (x['ID_USER'], x['WEEK'])
+    value = []
+    if x['VolData'] > 0:
+        value += [(24 * n_features * x['DoW'] + n_features * x['HOUR'], x['VolData'])]
+    if x['NoSMS'] > 0:
+        value += [(24 * n_features * x['DoW'] + n_features * x['HOUR'] + 1, x['NoSMS'])]
+    if x['DurationVoice'] > 0:
+        value += [(24 * n_features * x['DoW'] + n_features * x['HOUR'] + 2, x['DurationVoice'])]
+    return key, value
+
+
+# map each record x to (key, value) with function parse()
+rdd = df.rdd.map(lambda x: parse(x))
+# concatenate values (concatenate list of (idx, val)) to sparse vector
+rdd = rdd.reduceByKey(lambda a, b: a + b)
+# create features is sparse vector
+rdd = rdd.map(lambda x: (x[0][0], x[0][1], DenseVector(SparseVector(24 * 7 * n_features, x[1]))))
+
+
+
+final_df = spark.createDataFrame(rdd, ['ID_USER', 'WEEK', 'FEATURES']).select('FEATURES')
+final_df.show()
+
+# write input to csv file
+final_df.toPandas().to_csv('input.csv', index=False)
 
 spark.stop()
 
 e = time.time()
-print("Time: %f s" % (e-s))
+print("Time: %f s" % (e - s))
+
+
+# class Preprocess:
+#     def __init__(self, data_dir):
+#         self
+#
+#     def run(self):
+#         # Initialize spark session
+#         spark = SparkSession \
+#             .builder \
+#             .appName("read_csv") \
+#             .getOrCreate()
